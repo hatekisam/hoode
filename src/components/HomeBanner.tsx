c98af644f132c029d9c0890f842e0bd2cb2f68d9
mugishayves <mugishayves189000@gmail.com>
2023-08-10 22:48:20 +0200

    Fixed the centering on the div

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -3,19 +3,43 @@ import { Link } from 'react-router-dom'
 
 const HomeBanner: React.FC = () => {
         const [mobileNav, setMobileNav] = useState(false)
+        const [changeLanguage,setChangeLanguage] =useState(false)
         return (
                 <div className='rounded-md w-full text-white bg-[#232323] px-[5vw] py-5 relative'>
                         <img src="/image/texture1.png" alt="" className="w-full h-full absolute  top-0 left-0" />
                         <img src="/image/phone1.png" alt="" className="w-[20vw] absolute  bottom-0 right-[28vw]" />
                         <img src="/image/phone2.png" alt="" className="w-[20vw] absolute  bottom-0 right-[5vw]" />
-                        <div className='flex flex-row justify-between items-center relative z-50'>
+                        <div className='flex flex-row justify-between items-center relative  p-[1vw] rounded-full text-white'>
                                 {mobileNav && (
-                                        <div className="block md:hidden absolute -bottom-[22vw] right-0 rounded-md  bg-[#232323] p-[2vw]">
-                                                <div className="flex flex-col gap-1 text-[1.5vw]">
+                                        <div className="block md:hidden absolute top-[110%] right-0 rounded-md  bg-[#232323] p-[2vw]  w-[70vw]">
+                                                {changeLanguage && (
+                                                        <div className='block md:hidden absolute rounded-md  bg-[#232323] p-[2vw] top-[103%] -left-10 w-[30vw]'>
+                                                                <div className="flex flex-row justify-between">
+                                                                        <div></div>
+                                                                        <button onClick={() => setChangeLanguage(false)} >
+                                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className='w-5 h-5 fill-white'>
+                                                                                        <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                                                                                </svg>
+                                                                        </button>
+                                                                </div>
+                                                                <button onClick={() => setChangeLanguage(false)} className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                        <p className='font-medium'>Arab</p>
+                                                                </button>
+                                                                <button onClick={() => setChangeLanguage(false)} className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                        <p className='font-medium'>Eng(USA)</p>
+                                                                </button>
+                                                                <button onClick={() => setChangeLanguage(false)} className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                        <p className='font-medium'>Eng(UK)</p>
+                                                                </button>
+                                                                <button onClick={() => setChangeLanguage(false)} className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                        <p className='font-medium'>Arab</p>
+                                                                </button>
+                                                        </div>)}
+                                                <div className="flex flex-col gap-1  text-sm w-full">
                                                         <div className="flex flex-row justify-between">
                                                                 <div></div>
                                                                 <button onClick={() => setMobileNav(false)}>
-                                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"  className='w-5 h-5 fill-white'>
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className='w-5 h-5 fill-white'>
                                                                                 <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                                                                         </svg>
                                                                 </button>
@@ -23,12 +47,12 @@ const HomeBanner: React.FC = () => {
                                                         <Link to={"/about"}>About us</Link>
                                                         <Link to={"/blog"}>Blog</Link>
                                                         <Link to={"/contact-us"}>Contact us</Link>
-                                                        <div className='flex flex-row gap-3 items-center  '>
-                                                                <div className="flex flex-row gap-3 items-center">
-                                                                        <img src="/image/usa.png" alt="United States English" className='w-[2vw]' />
+                                                        <div className='flex flex-row  items-center  justify-between '>
+                                                                <button className="flex flex-row gap-3 items-center  py-[0.8vh]  px-[2vw]" onClick={() => setChangeLanguage(true)}>
+                                                                        <img src="/image/usa.png" alt="United States English" />
                                                                         <p className='font-medium'>Eng</p>
-                                                                </div>
-                                                                <button className='text-black font-medium bg-[#9EE970] py-[1.2vh]  px-[2vw] text-[1vw] rounded-full'>Become a Provider</button>
+                                                                </button>
+                                                                <button className='text-black font-medium bg-[#9EE970] py-[0.8vh]  px-[2vw]  rounded-full'>Become a Provider</button>
                                                         </div>
                                                 </div>
                                         </div>
@@ -38,12 +62,33 @@ const HomeBanner: React.FC = () => {
                                         <Link to={"/blog"}>Blog</Link>
                                         <Link to={"/contact-us"}>Contact us</Link>
                                 </div>
-                                <div className="flex flex-row gap-2">
-
-                                        <img src='/svg/logo.svg' alt='logo' />
-                                </div>
+                                <img src='/svg/logo.svg' alt='logo' className='w-[10vw]' />
                                 <div className='hidden md:flex flex-row gap-3 items-center text-[10px] sm:text-base   '>
-                                        <div className="flex flex-row gap-3 items-center">
+                                        {changeLanguage && (
+                                                <div className='hidden md:block absolute rounded-md  bg-[#232323] p-[1vw] pr-[3vw] top-[110%] right-[10vw] w-[40vw]'>
+                                                        <div className="flex flex-row justify-between">
+                                                                <div></div>
+                                                                <button onClick={() => setChangeLanguage(false)}>
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className='w-5 h-5 fill-white'>
+                                                                                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                                                                        </svg>
+                                                                </button>
+                                                        </div>
+                                                        <div className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                <p className='font-medium'>Arab</p>
+                                                        </div>
+                                                        <div className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                <p className='font-medium'>Eng(USA)</p>
+                                                        </div>
+                                                        <div className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                <p className='font-medium'>Eng(UK)</p>
+                                                        </div>
+                                                        <div className="flex flex-row gap-3 items-center p-1  rounded-md ">
+                                                                <p className='font-medium'>Arab</p>
+                                                        </div>
+                                                </div>
+                                        )}
+                                        <div className="flex flex-row gap-3 items-center p-1 hover:border rounded-md transition-all  relative" onClick={() => setChangeLanguage(true)}>
                                                 <img src="/image/usa.png" alt="United States English" />
                                                 <p className='font-medium'>Eng</p>
                                         </div>
